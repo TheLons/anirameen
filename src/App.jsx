614fd58e6ed54521514d901caa4731f53a128592
boris9606 <boristskhay@gmail.com>
2025-10-12 23:01:38 +0900

    added HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom" 
+import { HashRouter as Router, Route, Routes } from "react-router-dom" 
 import './App.css'
 
 import MainPage from './Components/MainPage/MainPage'
