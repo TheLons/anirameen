92c6cc9809e2ade137570fdbe7001c8285fa85e6
boris9606 <boristskhay@gmail.com>
2025-10-13 01:41:10 +0900

    deploy color build test

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', 
+  build: {
+    outDir: 'dist', // папка сборки
+    emptyOutDir: true, // очищать папку перед сборкой
+  },
 })
